e3d0bbfe6cc14de7f5f521b95ed6e22fc9720cb6
JP Angelle <jpangelle15@gmail.com>
2018-02-22 16:59:55 -0600

    pulling

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -53,6 +53,7 @@ class App extends React.Component {
 
   //this function asks the server to get trump's tweets from the db and send them here to display
   getTrumpTweetsFromDb() {
+    console.log('getting')
     axios.get('/getTrumpTweets/db')
     .then(res => {
       console.log(res.data)
