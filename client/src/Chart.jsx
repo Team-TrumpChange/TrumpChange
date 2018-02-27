cec7a59196726eff4ee03485d2ffcd2a5a930d3c
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-02-26 21:18:25 -0600

    color changes

--- client/src/Chart.jsx
+++ client/src/Chart.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ResponsiveContainer, RadialBarChart, RadialBar, Legend} from 'recharts';
-import { redA200, redA500, blueA200, blue300, greenA100, grey300, blueA100, fullWhite, fullBlack, darkBlack, white, redA100 } from 'material-ui/styles/colors';
+import {
+  blue200, blue400, blue600, fullWhite, fullBlack, darkBlack, white } from 'material-ui/styles/colors';
 import Paper from 'material-ui/Paper';
 class Chart extends Component {
 
@@ -18,10 +19,12 @@ class Chart extends Component {
    
   	return (
         <ResponsiveContainer>
-          <RadialBarChart innerRadius='50%' outerRadius='100%' 
-          data={[{ name: 'TrumpTweets', number: totalUsers, fill: redA100 }, 
-          { name: 'People Contributing to Change', number: totalNumTweets, fill: blueA100 },
-          { name: 'Number of Donations', number: totalDonated, fill: greenA100 },]} 
+          <RadialBarChart innerRadius='55%' outerRadius='100%' 
+          data={[{
+            name: 'Number of Donations', number: totalDonated, fill: blue200 },
+          { name: 'TrumpTweets', number: totalUsers, fill: blue400 },
+          { name: 'People Contributing to Change', number: totalNumTweets, fill: blue600 }
+          ]} 
           startAngle={180} endAngle={-179}>
             <RadialBar minAngle={0} label={{ fill: '#ffffff', position: 'insideStart'}} background clockWise={true} dataKey= 'number' />
             <Legend layout='vertical' verticalAlign='top' wrapperStyle={style}/>
